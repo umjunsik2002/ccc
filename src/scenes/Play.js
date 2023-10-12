ca7387fe2865ea2ee69e692472370a6f4ba5e26f
sarowley <sarowley@ucsc.edu>
2023-10-12 15:35:21 -0700

    added sphere that rotates

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,7 +2,9 @@ class Play extends Phaser.Scene {
   constructor() {
     super("playScene");
   }
-  preload() {}
+  preload() {
+    this.load.image("circle", "assets/Sprite-0002.png");
+  }
   create() {
     let menuConfig = {
       fontFamily: "Courier",
@@ -15,10 +17,22 @@ class Play extends Phaser.Scene {
       fixedWidth: 0,
     };
 
+    //keybinds
+    keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+
     //adding text
-    this.add.text(225, 300, "you are in play", menuConfig);
+    //this.add.text(225, 300, "you are in play", menuConfig);
+
+    //this.r1 = this.add.circle(1920 / 2, 1080 / 2, 325, 0x6666ff);
+    this.r1 = this.add.image(1920 / 2, 1080 / 2, "circle");
   }
   update() {
     //console.log("test")
+    //if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
+    if (keyENTER.isDown) {
+      console.log("down");
+      this.r1.rotation += 0.01;
+      //also make the cat walk here
+    }
   }
 }
