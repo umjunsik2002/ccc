0d56fa98bd1076c8914cd71e6fc115261d89b376
umjunsik <sooin0514@gmail.com>
2023-10-12 15:17:49 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ let config = {
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1920,
-        height: 1080,
+        width: 1024,
+        height: 768,
         physics:{
             default: "arcade",
             arcade : {
