ed964173da9415ecaa9a6e6d1dff54f0af11f304
sarowley <sarowley@ucsc.edu>
2023-10-12 14:56:32 -0700

    used prettier

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
 let config = {
-    type: Phaser.AUTO,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1920,
-        height: 1080,
-        physics:{
-            default: "arcade",
-            arcade : {
-                //debug: true
-            }
-        },
+  type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1920,
+    height: 1080,
+    physics: {
+      default: "arcade",
+      arcade: {
+        //debug: true
+      },
     },
-    backgroundColor: '#000000',
-    scene: [Title, Play]
-}
+  },
+  backgroundColor: "#000000",
+  scene: [Title, Play],
+};
 
 let game = new Phaser.Game(config);
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,24 +1,24 @@
-class Play extends Phaser.Scene{
-    constructor(){
-        super('playScene');
-    }
-    preload(){}
-    create(){
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '100px',
-            color: '#FFFFFF',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
+class Play extends Phaser.Scene {
+  constructor() {
+    super("playScene");
+  }
+  preload() {}
+  create() {
+    let menuConfig = {
+      fontFamily: "Courier",
+      fontSize: "100px",
+      color: "#FFFFFF",
+      padding: {
+        top: 5,
+        bottom: 5,
+      },
+      fixedWidth: 0,
+    };
 
-        //adding text
-        this.add.text(225, 300, 'you are in play', menuConfig);
-    }
-    update(){
-        //console.log("test")
-    }
+    //adding text
+    this.add.text(225, 300, "you are in play", menuConfig);
+  }
+  update() {
+    //console.log("test")
+  }
 }
